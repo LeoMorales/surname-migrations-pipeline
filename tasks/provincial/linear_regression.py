f363a1aaa2c6bd45db3b1cf77611e3c41227c7e9
LeoMorales <leo.moralesr23@gmail.com>
2023-10-17 19:41:44 -0300

    refactor: obtención de parámetros de la regresion lineal

--- tasks/provincial/linear_regression.py
+++ tasks/provincial/linear_regression.py
@@ -1,5 +1,6 @@
 import pandas
-from surnames_package import utils, isonymic, isonymic_vis, models
+from surnames_package import utils, models_utils
+
 import matplotlib.pyplot as plt
 
 
@@ -22,29 +23,38 @@ def get_linear_regression_params_2015(upstream, product):
     df = pandas.read_parquet(str(upstream["get-surnames-2015"]))
     df = utils.append_cell_description(df, departmentCodeColumn="province_id")
 
-    provinces = (
-        df[["province_id", "provincia_nombre"]]
-        .drop_duplicates()
-        .to_records(index=False)
+    output_df = models_utils.compute_linear_regression_params_for_groups(
+        df, division_id_column="province_id", division_name_column="provincia_nombre"
     )
-    param_list = []
 
-    for province_id, province_name_i in provinces:
-        province_df = (
-            df[df["provincia_nombre"] == province_name_i].reset_index(drop=True).copy()
-        )
+    output_df["division_level"] = "province"
+    output_df["year"] = "2015"
+    output_df.to_parquet(str(product))
 
-        occur_vs_freq_df = isonymic.getOccurrencesVsFrequencies(province_df["surname"])
 
-        params_for_region_i = models.get_linear_regression_params(
-            X=occur_vs_freq_df["occurrences_log"], y=occur_vs_freq_df["frecuency_log"]
-        )
+def get_linear_regression_params_2021(upstream, product):
+    """Retorna los parámetros resultantes de aplicar OLS a las variables X='occurrences_log' e y='frequency_log'.
 
-        params_for_region_i["division_level"] = "province"
-        params_for_region_i["division_name"] = province_name_i
-        params_for_region_i["division_id"] = province_id
+    Args:
+        upstream (_type_): _description_
+        product (_type_): Dataframe con las columnas:
+            intercept,
+            slope,
+            r_square,
+            r_square_adj,
+            f_score,
+            division_level,
+            division_name,
+            division_id
 
-        param_list.append(params_for_region_i)
+    """
+    df = pandas.read_parquet(str(upstream["get-surnames-2021"]))
+    df = utils.append_cell_description(df, departmentCodeColumn="province_id")
 
-    output_df = pandas.DataFrame(param_list)
+    output_df = models_utils.compute_linear_regression_params_for_groups(
+        df, division_id_column="province_id", division_name_column="provincia_nombre"
+    )
+
+    output_df["division_level"] = "province"
+    output_df["year"] = "2021"
     output_df.to_parquet(str(product))
