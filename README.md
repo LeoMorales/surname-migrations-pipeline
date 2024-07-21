315bc1f717d75cbd287992484f8ef949933e9fbe
LeoMorales <leo.moralesr23@gmail.com>
2024-07-21 14:51:15 -0300

    actualiza archivo readme y sus dependencias

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Migration pipeline
 
+Repositorio del proyecto de investigación denominado:
+"Migraciones internas en Argentina: estudio demográfico a partir de apellidos".
+
 ![pipeline_img](./static/migracion-pipeline.png)
+
+## Setup
+
+Using ploomber pipelines:
+
+```
+> ./install_packages.sh
+> ploomber build
+```
+
+Inspect `_output`/`_products` folders.
--- tasks/isonymic_indicators.py
+++ tasks/isonymic_indicators.py
@@ -1,33 +1,32 @@
 import pandas
 from surnames_package import isonymic
-from termcolor import colored
 
 
 def get_isonymy_departments_2015(product):
     """"""
     print("Computing get_isonymy_departments_2015...")
-    df = pandas.read_parquet("/home/lmorales/work/pipelines/surname_data_pipeline/_products/clean/surnames_2015.parquet")
-    #df = pandas.read_parquet(surname_data_path)
-    
-    output_df = isonymic.get_isonymic_data_by_cells(
-        df,
-        slice_by_column='department_id',
-        surname_column='surname'
+    df = pandas.read_parquet(
+        "/home/lmorales/work/pipelines/surname_data_pipeline/_products/clean/surnames_2015.parquet"
     )
-    output_df = output_df.rename(columns={'cell_id': 'department_id'})
+    # df = pandas.read_parquet(surname_data_path)
+
+    output_df = isonymic.get_isonymic_data_by_cells(
+        df, slice_by_column="department_id", surname_column="surname"
+    )
+    output_df = output_df.rename(columns={"cell_id": "department_id"})
     output_df.to_parquet(str(product))
+
 
 def get_isonymy_departments_2021(product):
     """"""
     print("Computing get_isonymy_departments_2021...")
-    df = pandas.read_parquet("/home/lmorales/work/pipelines/surname_data_pipeline/_products/clean/surnames_2021.parquet")
-    #df = pandas.read_parquet(surname_data_path)
-    
-    output_df = isonymic.get_isonymic_data_by_cells(
-        df,
-        slice_by_column='department_id',
-        surname_column='surname'
+    df = pandas.read_parquet(
+        "/home/lmorales/work/pipelines/surname_data_pipeline/_products/clean/surnames_2021.parquet"
     )
-    output_df = output_df.rename(columns={'cell_id': 'department_id'})
-    output_df.to_parquet(str(product))
+    # df = pandas.read_parquet(surname_data_path)
 
+    output_df = isonymic.get_isonymic_data_by_cells(
+        df, slice_by_column="department_id", surname_column="surname"
+    )
+    output_df = output_df.rename(columns={"cell_id": "department_id"})
+    output_df.to_parquet(str(product))
